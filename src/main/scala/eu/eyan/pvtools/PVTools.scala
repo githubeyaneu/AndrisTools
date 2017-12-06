4a863d1081cf1250d4d0faeab56895d615a4324d
githubeyaneu <github@eyan.eu>
2017-12-06 16:07:42 +0100

    PVTools ver1

---
+++ src/main/scala/eu/eyan/pvtools/PVTools.scala
@@ -0,0 +1,130 @@
+package eu.eyan.pvtools
+
+import eu.eyan.util.swing.JPanelWithFrameLayout
+import eu.eyan.log.Log
+import eu.eyan.log.LogWindow
+import eu.eyan.util.awt.clipboard.ClipboardPlus
+import eu.eyan.util.io.FilePlus.FilePlusImplicit
+import eu.eyan.util.java.net.URLPlus.URLImplicit
+import eu.eyan.util.registry.RegistryPlus
+import eu.eyan.util.scala.TryCatch
+import eu.eyan.util.scala.collection.TraversableOncePlus.TraversableOnceImplicit
+import eu.eyan.util.string.StringPlus.StringPlusImplicit
+import eu.eyan.util.swing.JButtonPlus.JButtonImplicit
+import eu.eyan.util.swing.JCheckBoxPlus.JCheckBoxImplicit
+import eu.eyan.util.swing.JFramePlus.JFramePlusImplicit
+import eu.eyan.util.swing.JLabelPlus.JLabelImplicit
+import eu.eyan.util.swing.JPanelWithFrameLayout
+import eu.eyan.util.swing.JTextAreaPlus.JTextAreaImplicit
+import eu.eyan.util.swing.JTextFieldPlus.JTextFieldPlusImplicit
+import eu.eyan.util.swing.SwingPlus
+import javax.swing.JFrame
+import javax.swing.JOptionPane
+import java.awt.Color
+import java.awt.Desktop
+import java.net.URI
+import java.net.URLEncoder
+import scala.io.Source
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.Locale
+import java.time.ZoneId
+import eu.eyan.util.java.time.InstantPlus.InstantImplicit
+import java.io.File
+import java.util.concurrent.ScheduledThreadPoolExecutor
+import eu.eyan.util.awt.AwtHelper
+import java.util.concurrent.TimeUnit
+
+object PVTools extends App {
+  Log.activateInfoLevel.redirectSystemOutAndError
+  val TITLE = "Photo and video import"
+
+  val panel = new JPanelWithFrameLayout().withBorders.withSeparators
+    .newColumn.newColumnFPG
+
+  panel.newRow.addLabel("Import path: ")
+  val importPathTextField = panel.nextColumn.addTextField("").rememberValueInRegistry("importPath")
+
+  panel.newRow.addLabel("Export path: ")
+  val exportPathTextField = panel.nextColumn.addTextField("").rememberValueInRegistry("exportPath")
+
+  panel.newRow.addLabel("Files to import: ")
+  val extensionsToImportTextField = panel.nextColumn.addTextField("JPG,MTS").rememberValueInRegistry("extensionsToImport")
+
+  panel.newRow.addLabel("Files to convert: ")
+  val extensionsToConvertTextField = panel.nextColumn.addTextField("MTS").rememberValueInRegistry("extensionsToConvert")
+
+  panel.newRow.addLabel("ffmpeg.exe location: ")
+  val ffmpegPathTextField = panel.nextColumn.addTextField("""C:\private\ffmpeg\bin\ffmpeg.exe""").rememberValueInRegistry("extensionsToConvert")
+
+  panel.newRow.addButton("Check to import").onAction_disableEnable(filesToImport)
+  val checkToImportLabel = panel.nextColumn.addLabel("")
+
+  panel.newRow.addButton("Import").onAction_disableEnable(importFiles)
+  val importLabel = panel.nextColumn.addLabel("")
+
+  val frame= new JFrame().title(TITLE).onCloseHide.iconFromChar('I', Color.YELLOW).addToSystemTray().withComponent(panel)
+    .menuItem("File", "Exit", System.exit(0))
+    .menuItem("Debug", "Open log window", LogWindow.show(panel))
+    .menuItem("Debug", "Copy logs to clipboard", ClipboardPlus.copyToClipboard(Log.getAllLogs))
+    .menuItem("Debug", "Clear registry values", RegistryPlus.clear(TITLE))
+    .menuItem("Help", "Write email", writeEmail)
+    .menuItem("Help", "About", alert("This is not an official tool, no responsibilities are taken. Use it at your own risk."))
+    .packAndSetVisible
+
+  val pool = new ScheduledThreadPoolExecutor(1)
+  pool.scheduleAtFixedRate(AwtHelper.runnable(filesToImport), 0, 5, TimeUnit.SECONDS)
+  
+  def writeEmail =
+    Desktop.getDesktop.mail(new URI("mailto:PVTools@eyan.eu?subject=Photo%20and%20video%20import&body=" + URLEncoder.encode(Log.getAllLogs, "utf-8").replace("+", "%20")))
+
+  def convertVideo(in: File, out: File) = {
+    val targetVideo = out.withoutExtension + ".mp4"
+    Log.info("Converting " + in + " to " + targetVideo)
+    val convertBat = s"""${ffmpegPathTextField.getText} -i "$in" -vf yadif -vcodec mpeg4 -b:v 17M -acodec libmp3lame -b:a 192k "$targetVideo""""
+    convertBat.executeAsBatchFile()
+    true
+  }
+
+  def isVideoToConvert(file: File) = file.endsWith(extensionsToConvertTextField.getText.split(","): _*)
+
+  def convertOrCopy(fileToImport: File, targetFile: File) =
+    if (isVideoToConvert(fileToImport)) convertVideo(fileToImport, targetFile)
+    else fileToImport.copyTo(targetFile)
+
+  def importFiles = {
+    importLabel.text("Importing")
+    val importedFiles = for (fileToImport <- filesToImport) yield {
+      val fileName = fileToImport.getName
+      val fileDateTime = fileToImport.lastModifiedTime.toString("yyyyMMdd_HHmmss")
+      val targetFile = (exportPathTextField.getText + "\\" + fileDateTime + " " + fileName).asFile
+      Log.info("Convert or copy " + fileToImport + " to " + targetFile)
+      if (convertOrCopy(fileToImport, targetFile)) Option(fileToImport) else None
+    }
+    val newList = alreadyImportedFiles ++ importedFiles.flatten
+    newList.mkStringNL.writeToFile(alreadyImportedFile)
+    importLabel.text("Import finished")
+  }
+
+  def alreadyImportedFile = (exportPathTextField.getText + "\\alreadyImported.txt").asFile
+  def alreadyImportedFiles = (if (alreadyImportedFile.existsAndFile) Source.fromFile(alreadyImportedFile).getLines.toList else List()).map(_.asFile)
+
+  def filesToImport = {
+    Log.info("Checking files to import")
+    if (!importPathTextField.getText.asDir.existsAndDir) { alert("Import dir does not exists."); List() }
+    else if (!exportPathTextField.getText.asDir.existsAndDir) { alert("Export dir does not exists."); List() }
+    else if (ffmpegPathTextField.getText.asFile.notExists) { alert("ffmpeg does not exists."); List() }
+    else {
+      val allFiles = importPathTextField.getText.asDir.subFiles.toList.filter(_.endsWith(extensionsToImportTextField.getText.split(","): _*))
+      val filesToImport = allFiles.diff(alreadyImportedFiles)
+
+      checkToImportLabel.setText(filesToImport.size + " files to import")
+      Log.info("Checking files to import: " + filesToImport.size + " files.")
+      if(filesToImport.size>0) frame.state_Normal.visible.toFront
+      
+      filesToImport
+    }
+  }
+
+  def alert(msg: String) = JOptionPane.showMessageDialog(null, msg)
+}
