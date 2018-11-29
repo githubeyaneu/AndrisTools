3b856c80ff24b62304b71e5f982e713eded745f6
Andras Frey <extern.andras.frey@elektrobit.com>
2018-11-29 14:35:30 +0100

    link

--- src/main/scala/eu/eyan/bt/KekFog.scala
+++ src/main/scala/eu/eyan/bt/KekFog.scala
@@ -6,5 +6,7 @@ import javax.bluetooth.LocalDevice
 
 object KekFog extends App {
 //  new JFrame().withComponent(panel)
+  //http://www.aviyehuda.com/blog/2010/01/08/connecting-to-bluetooth-devices-with-java/
+  println(LocalDevice.isPowerOn)
   val localDevice = LocalDevice.getLocalDevice
 }
